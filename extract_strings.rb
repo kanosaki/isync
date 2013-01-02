985261781860b83bf29dd5ef5a7bba5666a820cd
kanosaki <kanosaki@gmail.com>
2013-01-02 16:45:52 +0900

    Preserve and transfer previous configured language files

--- extract_strings.rb
+++ extract_strings.rb
@@ -1,18 +1,44 @@
 #!/usr/bin/env ruby
 require 'json'
+require 'psych'
 
 GETTEXT_FUNC = "_i"
 PATTERN = /(?<=#{GETTEXT_FUNC}\()(?:"|')(.+)(?:"|')/
 LANG_NAME = '<lang_name>'
+LANG_DIR = 'lang'
+LANG_FILE_EXT = '.json'
+INITIAL_STRING = '=============='
 def extract(file)
   result = {}
   file.each_line do |line|
     if PATTERN =~ line 
-      result[$1] = ""
+      result[$1] = INITIAL_STRING
     end
   end
-  { LANG_NAME => result }
+  result
 end
 
-puts JSON.pretty_generate(extract(ARGF))
+
+def check_unsed_msg(prev, current, name)
+    unused_msgs = prev.keys - current.keys
+    unless unused_msgs.empty?
+        $stderr.puts "At #{name}:"
+        unused_msgs.each do |msg|
+            $stderr.puts "WARN: Message #{msg} is no longer used!"
+        end
+    end
+end
+
+extracted = extract(ARGF)
+result = {}
+Dir.glob(LANG_DIR + '/*').each do |path|
+    prev = Psych.load_file(path)
+    check_unsed_msg(prev, extracted, path)
+    updated = extracted.dup.update(prev)
+    localename = File.basename(path, LANG_FILE_EXT)
+    File.write(path, JSON.pretty_generate(updated))
+    result[localename] = updated
+end
+
+puts JSON.pretty_generate(result)
 
