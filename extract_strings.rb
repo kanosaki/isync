0f14497a059e2bb1f288a9632f88249553c70990
kanosaki <kanosaki@gmail.com>
2013-01-02 16:06:53 +0900

    Modify extraction strategy

--- extract_strings.rb
+++ extract_strings.rb
@@ -1,9 +1,9 @@
 #!/usr/bin/env ruby
 require 'json'
 
-PATTERN = /(?<!_i\()\"(.+?)\"/
+GETTEXT_FUNC = "_i"
+PATTERN = /(?<=#{GETTEXT_FUNC}\()(?:"|')(.+)(?:"|')/
 LANG_NAME = '<lang_name>'
-
 def extract(file)
   result = {}
   file.each_line do |line|
